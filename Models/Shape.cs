4e620d08f4bd318915d2d5557c6bfc4da477497c
Mitch Graff <Mitch.Graff@cropins.net>
2020-09-23 13:54:26 -0400

    Add project files.

---
+++ Models/Shape.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Models
+{
+    public class Shape
+    {
+        private string shapeName;
+        public string ShapeName
+        {
+            get { return shapeName; }
+            set { shapeName = value; }
+        }
+
+        private string shapeColor;
+        public string ShapeColor
+        {
+            get { return shapeColor; }
+            set { shapeColor = value; }
+        }
+
+        private double shapeSize;
+        public double ShapeSize
+        {
+            get { return shapeSize; }
+            set
+            {
+                //minimum shapeSize = 1.0
+                if (value < 1)
+                    value = 1.0;
+                shapeSize = value;
+            }
+        }
+
+        public int ShapeX { get; set; }
+        public int ShapeY { get; set; }
+
+        public Shape(string sn)
+        {
+            ShapeName = sn;
+            ShapeColor = "red";
+            ShapeSize = 0;
+            ShapeX = 0;
+            ShapeY = 0;
+        }
+        public Shape(string sn, string co, double sz, int x, int y)
+        {
+            ShapeName = sn;
+            ShapeColor = co;
+            ShapeSize = sz;
+            ShapeX = x;
+            ShapeY = y;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("shape: ");
+            Console.WriteLine("  name: " + ShapeName);
+            Console.WriteLine("  color:" + ShapeColor);
+            Console.WriteLine("  size: " + ShapeSize);
+        }
+
+        public override string ToString()
+        {
+            return ShapeName;
+        }
+
+
+    }
+}
---
+++ Visuals/ShapeVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visuals
+{
+    public class ShapeVM : INotifyPropertyChanged
+    {
+        private string shapeName;
+        public string ShapeName
+        {
+            get { return shapeName; }
+            set
+            {
+                shapeName = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged == null)
+            {
+                return;
+            }
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
